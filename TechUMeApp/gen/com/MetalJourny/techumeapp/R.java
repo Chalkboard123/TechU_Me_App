aab6ae620949db11e553efa864c836cfe2751804
s211729 <s211729@SPHA307M07.ocsb.local>
2013-12-03 09:33:01 -0500

    metal journey

--- TechUMeApp/gen/com/MetalJourny/techumeapp/R.java
+++ TechUMeApp/gen/com/MetalJourny/techumeapp/R.java
@@ -24,7 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int button1=0x7f080002;
+        public static final int button10=0x7f08000b;
+        public static final int button11=0x7f08000c;
+        public static final int button12=0x7f08000d;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f080006;
+        public static final int button6=0x7f080007;
+        public static final int button7=0x7f080008;
+        public static final int button8=0x7f080009;
+        public static final int button9=0x7f08000a;
+        public static final int editText1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,8 +47,21 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int ConversionSteps=0x7f05000f;
+        public static final int MetalJourney=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int b0=0x7f05000d;
+        public static final int b2=0x7f050005;
+        public static final int b3=0x7f050006;
+        public static final int b4=0x7f050007;
+        public static final int b5=0x7f050008;
+        public static final int b6=0x7f050009;
+        public static final int b7=0x7f05000a;
+        public static final int b8=0x7f05000b;
+        public static final int b9=0x7f05000c;
+        public static final int bequal=0x7f05000e;
+        public static final int button1=0x7f050004;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
